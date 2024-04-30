4fdd6c03615a534250b54b9b9e35aa2b4d593106
Stephen Walther <Stephen.Walther@superexpert.com>
2024-04-29 19:36:56 -0700

    Seed database

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,10 +1,33 @@
+const { db } = require('@vercel/postgres');
+
+async function seedSitemaps(client) {
+  try {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+    // Create the "users" table if it doesn't exist
+    const createTable = await client.sql`
+      CREATE TABLE IF NOT EXISTS sitemaps (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        sitemap TEXT NOT NULL,
+        created timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP
+      );
+    `;
+
+    console.log(`Created "sitemaps" table`);
+
+
+    return {
+      createTable,
+    };
+  } catch (error) {
+    console.error('Error seeding users:', error);
+    throw error;
+  }
+}
+
 async function main() {
     const client = await db.connect();
   
-    await seedUsers(client);
-    await seedCustomers(client);
-    await seedInvoices(client);
-    await seedRevenue(client);
+    await seedSitemaps(client);
   
     await client.end();
   }
